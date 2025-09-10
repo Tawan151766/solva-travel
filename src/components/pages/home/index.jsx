850a9abd4697e7158120d68c15d095eaec0dc6ee
Tawan151766 <khumta15176@gmail.com>
2025-09-10 13:29:38 +0700

    fix: TravelPackages

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -11,13 +11,12 @@ export function Home() {
       <div className="relative flex w-full min-h-screen flex-col bg-gradient-to-br from-black via-[#0a0804] to-black overflow-x-hidden font-['Plus_Jakarta_Sans','Noto_Sans',sans-serif]">
         {/* Luxury Background Overlay */}
         <div className="absolute inset-0 bg-gradient-to-br from-[#FFD700]/5 via-transparent to-[#FFD700]/5 opacity-50"></div>
-        
+
         <div className="layout-container flex h-full grow flex-col relative">
           <div className="flex flex-1 justify-center py-5 px-4 sm:px-6 lg:px-8">
             <div className="layout-content-container flex flex-col max-w-6xl flex-1">
               <SearchFilters />
               <TravelPackages />
-              {/* Pagination ปิดไว้ก่อน เพื่อความง่าย */}
               {/* <Pagination /> */}
             </div>
           </div>
